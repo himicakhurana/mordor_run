507de12a9f36c33ad4ac9d80b0af0f812e50ad17
Himica Khurana <himicakhurana@gmail.com>
2021-04-17 15:47:04 -0700

    Update scratch_pad.py

--- scratch_pad.py
+++ scratch_pad.py
@@ -8,3 +8,6 @@ sprite2.image.fill((0, 255, 0))
 sprite2.rect = pygame.Rect(*screen.get_rect().center, 0, 0).inflate(75, 75)
 all_group = pygame.sprite.Group([sprite2, sprite1])
 test_group = pygame.sprite.Group(sprite2)
+
+#inside loop
+all_group.draw(win)
