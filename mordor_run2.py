659851b3adbf4ee5a3007d91686d87beedd9e9d7
schuylerb22 <schuylerb22@MacBook-Air.localdomain>
2021-04-24 15:59:33 -0700

    Adding images- obstacles

--- mordor_run2.py
+++ mordor_run2.py
@@ -19,6 +19,7 @@ screen = pygame.display.set_mode(size, HWSURFACE | DOUBLEBUF | RESIZABLE)
 # Set title to the window
 pygame.display.set_caption("Mordor Run")
 
+
 dead=False
 path="/Users/schuylerb22/Desktop/thumb-1920-85585.jpg"
 clock = pygame.time.Clock()
@@ -26,47 +27,45 @@ background_image = pygame.image.load(path).convert()
 x, y = 250, 200
 font = pygame.font.Font('freesansbold.ttf', 32)
 text = font.render('Score : 0', True, GREEN, BLUE)
+health_text = font.render('Health : 100%', True, GREEN, BLUE)
 textRect = text.get_rect()
 textRect.center = (100,100)
+health_text_rect = health_text.get_rect()
+health_text_rect.center = (500,100)
 sprite1 = pygame.sprite.Sprite()
 sprite1.image = pygame.Surface((75, 75))
 sprite1.image.fill((255, 0, 0))
 sprite1.rect = pygame.Rect(*screen.get_rect().center, 0, 0).inflate(75, 75)
-sprite2 = pygame.sprite.Sprite()
-sprite2.image = pygame.Surface((75, 75))
-sprite2.image.fill((0, 255, 0))
-sprite2.rect = pygame.Rect(*screen.get_rect().center, 0, 0).inflate(75, 75)
-all_group = pygame.sprite.Group([sprite2, sprite1])
+obstacle1_sprite = pygame.sprite.Sprite()
+#sprite2.image.fill((0, 255, 0))
+#sprite2.rect = pygame.Rect(*screen.get_rect().center, 0, 0).inflate(75, 75)
+obstacle1_sprite.image = pygame.image.load("Untitled_Artwork 3.png").convert()
+obstacle1_sprite.image = pygame.transform.scale(obstacle1_sprite.image, (75,75))
+obstacle1_sprite.image = obstacle1_sprite.image.convert_alpha()
+obstacle1_sprite.rect = obstacle1_sprite.image.get_rect()
+health_sprite = pygame.sprite.Sprite()
+health_sprite.image = pygame.Surface((75, 75))
+health_sprite.image.fill((3, 252, 232))
+health_sprite.rect = pygame.Rect(*screen.get_rect().center, 0, 0).inflate(75, 75)
+all_group = pygame.sprite.Group([obstacle1_sprite, sprite1, health_sprite])
 iter=0
-test_group = pygame.sprite.Group(sprite2)
+test_group = pygame.sprite.Group(obstacle1_sprite)
+health_group = pygame.sprite.Group(health_sprite)
+health=100
 while(dead==False):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             dead = True
         pressed = pygame.key.get_pressed()
-        if pressed[K_UP]:
-            if(y-10)>0:
-                y = y - 10
-            else:
-               print("You are on edge.")
-        if pressed[K_DOWN]:
-            if(y+10)<800:
-                
-                    y = y + 10
-            else:
-                print("You are on edge.")
-        if pressed[K_LEFT]:
-            x = x - 10
-        if pressed[K_RIGHT]:
-            x = x + 10
-        if x>=800 or x<=0:
-            print("You're outside the window!")
-        if y>=800 or y<=0:
-            print("You're outside the window!")
+
     #print("Game is running")
     #screen.blit(background_image, [0, 0])
-    screen.blit(pygame.transform.scale(background_image, (800, 800)), (0, 0))
+    #screen.blit(pygame.transform.scale(background_image, (800, 800)), (0, 0))
+    screen.fill((255,255,255))
+    health_message='Health : '+str(health)+'%'
+    health_text = font.render(health_message, True, GREEN, BLUE)
     screen.blit(text, textRect)
+    screen.blit(health_text, health_text_rect)
     #pygame.draw.polygon(screen, BLACK, [[x+10, y+10], [x+0, y+20], [x+20, y+20]], 5)
     #inside loop
     sprite1.rect.centerx = x
@@ -74,13 +73,18 @@ while(dead==False):
     all_group.draw(screen)
     iter+=1
     if iter % 2 == 0:
-        sprite2.rect.centerx = sprite2.rect.centerx + randint(1, 20)
-        sprite2.rect.centery = sprite2.rect.centery + randint(1, 20)
+        obstacle1_sprite.rect.centerx = obstacle1_sprite.rect.centerx + randint(80, 100)
+        obstacle1_sprite.rect.centery = obstacle1_sprite.rect.centery + randint(80, 100)
     else:
-        sprite2.rect.centerx = sprite2.rect.centerx - randint(1, 20)
-        sprite2.rect.centery = sprite2.rect.centery - randint(1, 20)
+        obstacle1_sprite.rect.centerx = obstacle1_sprite.rect.centerx - randint(80, 100)
+        obstacle1_sprite.rect.centery = obstacle1_sprite.rect.centery - randint(80, 100)
     collide = pygame.sprite.spritecollide(sprite1, test_group, False)
     for s in collide:
+        health=health-5
         print('collision')
+    health_collide = pygame.sprite.spritecollide(sprite1, health_group, False)
+    for s in health_collide:
+        health=health+5
+        print('health')
     pygame.display.flip()
     clock.tick(clock_tick_rate)
